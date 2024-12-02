3360de070137e273e61b2096052c31c26285ff34
wiardvasen <vaw@MacBook-Pro-van-wiardvasen-2.local>
2024-12-02 16:49:22 +0100

    Add Nostr-related directories and files

---
+++ nostr/events/event_manager.py
@@ -0,0 +1,9 @@
+# Event Manager for Nostr
+# Handles processing and routing of Nostr events
+class EventManager:
+    def __init__(self):
+        pass
+
+    def process_event(self, event):
+        pass
+
